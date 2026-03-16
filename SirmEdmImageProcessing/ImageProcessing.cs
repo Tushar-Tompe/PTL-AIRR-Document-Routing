8e49d68e058e2f8e01fe7bbba94f38ae620e9af1
Tushar Tompe <Tushar.Tompe@Pilotthomas.com>
2026-03-16 12:47:09 -0500

    remove credential from log

--- SirmEdmImageProcessing/ImageProcessing.cs
+++ SirmEdmImageProcessing/ImageProcessing.cs
@@ -340,7 +340,7 @@ namespace Maxum.EDM
                 auth.DatabaseServer = _mySetings.Auth_DL_Server;
                 auth.LoginId = _mySetings.Auth_DL_User;
                 Session.RemotingEndPoint = _mySetings.DoclinkEndpoint;
-                Logger.Info("Step 3.8.4.2: ----------------Attempting to log into Doclink server: {Server} with User: {User} whose Database: {Database} and Doclink EndPoint: {Endpoint}", _mySetings.Auth_DL_Server, _mySetings.Auth_DL_User, _mySetings.Auth_DL_DB, _mySetings.DoclinkEndpoint, _mySetings.Auth_DL_PW);
+                Logger.Info("Step 3.8.4.2: ----------------Attempting to log into Doclink server: {Server} with User: {User} whose Database: {Database} and Doclink EndPoint: {Endpoint}", _mySetings.Auth_DL_Server, _mySetings.Auth_DL_User, _mySetings.Auth_DL_DB, _mySetings.DoclinkEndpoint);
 
                 Session.Login(auth, _mySetings.Auth_DL_PW);
                 Logger.Info("Step 3.8.4.3: Successfully logged into Doclink server: {Server}", _mySetings.Auth_DL_Server);
